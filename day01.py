bf360b0493356a702216090a230837061a15ffa8
ritikjshete <ritikjshete07@gmail.com>
2025-08-15 14:18:45 +0530

    Create day01.py
    done wit all test cases 
    works completely

---
+++ day01.py
@@ -0,0 +1,16 @@
+def sortlist(a):
+    if len(a) == 0:
+        return []
+    
+    zero, one, two = [], [], []
+    for i in a:
+        if i == 0:
+            zero.append(i)
+        elif i == 1:
+            one.append(i)
+        else:
+            two.append(i)
+
+    return zero + one + two
+
+print(sortlist([0, 1, 2, 1, 0, 2, 1, 0]))
