88a0cb4b4906a5a8f13cb0a613c575202225d740
ritikjshete <ritikjshete07@gmail.com>
2025-08-21 18:58:08 +0530

    Create day07.py

---
+++ day07.py
@@ -0,0 +1,24 @@
+def trapTwoPointer(height):
+    n = len(height)
+    if n < 3:
+        return 0
+
+    left, right = 0, n - 1
+    left_max, right_max = 0, 0
+    water = 0
+
+    while left < right:
+        if height[left] < height[right]:
+            if height[left] >= left_max:
+                left_max = height[left]
+            else:
+                water += left_max - height[left]
+            left += 1
+        else:
+            if height[right] >= right_max:
+                right_max = height[right]
+            else:
+                water += right_max - height[right]
+            right -= 1
+
+    return water
