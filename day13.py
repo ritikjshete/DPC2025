4edc082d65f386b59ce57a44054c3351dd1d9d7f
ritikjshete <ritikjshete07@gmail.com>
2025-09-01 06:32:01 +0530

    Create day13.py

---
+++ day13.py
@@ -0,0 +1,18 @@
+def longestPalindrome(s: str) -> str:
+    if len(s) < 2:
+        return s 
+    
+    start, max_len = 0, 1 
+    
+    def expandAroundCenter(left, right):
+        nonlocal start, max_len
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            if right - left + 1 > max_len:
+                start, max_len = left, right - left + 1
+            left -= 1
+            right += 1
+    
+    for i in range(len(s)):
+        expandAroundCenter(i, i)      
+        expandAroundCenter(i, i + 1)  
+    return s[start:start + max_len]
