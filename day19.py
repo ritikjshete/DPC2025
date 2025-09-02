2821ac02dd84401c16f059ffdac671c9513c2fdd
ritikjshete <ritikjshete07@gmail.com>
2025-09-02 19:17:12 +0530

    Create day19.py

---
+++ day19.py
@@ -0,0 +1,21 @@
+def evaluate_postfix(expression: str) -> int:
+    stack = []
+    operators = {"+", "-", "*", "/"}
+    
+    for token in expression.split():
+        if token not in operators:
+            stack.append(int(token))
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            
+            if token == "+":
+                stack.append(a + b)
+            elif token == "-":
+                stack.append(a - b)
+            elif token == "*":
+                stack.append(a * b)
+            elif token == "/":
+                stack.append(int(a / b))
+    
+    return stack[-1]
