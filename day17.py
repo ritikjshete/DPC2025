dfa0eeaf6e24b1c35c441d8aa0343ffca6f4177a
ritikjshete <ritikjshete07@gmail.com>
2025-09-02 19:26:54 +0530

    Create day17.py

---
+++ day17.py
@@ -0,0 +1,18 @@
+def prime_factorization(n: int) -> list:
+    factors = []
+
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+
+    i = 3
+    while i * i <= n:
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+        i += 2 
+        if n > 1:
+        factors.append(n)
+    
+    return factors
+
